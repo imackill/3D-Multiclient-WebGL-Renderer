15424fdcd7e97e89266699364336eba7e3aae51e
Iain MacKillop <iain.a.mackillop@gmail.com>
2023-03-01 18:17:40 -0500

    Working Cube, test is in main.js file for now

--- public/js/main.js
+++ public/js/main.js
@@ -7,16 +7,22 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 //camera position
-camera.position.z = 0;
+camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setClearColor( 0xffffff, 0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 $("body").append(renderer.domElement);
+
+//TEST LIGHTING ADD IN LIGHT OBJECT IN MODELS LATER!!!!
+const light = new THREE.AmbientLight(0xffaaff);
+light.position.set(10, 10, 10);
+scene.add(light);
 
 //test object (objName,pos,edgeLength,geometry,material,meshType,materialOptions,scene)
 let testCube = new models.immovableCube(
     "Test Cube",
-    {x:0,y:0,z:-5},
+    {x:0,y:0,z:0},
     1,
     THREE.BoxGeometry,
     THREE.MeshBasicMaterial,
@@ -26,8 +32,6 @@ let testCube = new models.immovableCube(
 );
 
 testCube.initElement();
-
-console.log(testCube.mesh);
 
 //final animation and rendering
 function animate() {
